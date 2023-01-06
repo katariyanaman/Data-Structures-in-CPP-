9c5a22a55222fad0b0fcf3ebde3365b988ad9d2c
Naman Katariya <109310617+katariyanaman@users.noreply.github.com>
2023-01-06 23:01:10 +0530

    Create Program_to_solve_tower_of_hanoi_using_binary_value.cpp

---
+++ Program_to_solve_tower_of_hanoi_using_binary_value.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+#include<conio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, x;
+    cout<<"\nEnter the No. of Disks: ";
+    cin>>n;
+    for (x = 1; x < (1 << n); x++)
+    {
+        printf("\nMove from Peg %i to Peg %i", (x&x-1)%3+1, ((x|x-1)+1)%3+1);
+    }
+    cout<<"\n";
+    getch();
+}
