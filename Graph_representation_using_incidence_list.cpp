f56be15eddab7a8b8c40e4fab39dc0fc8e298fba
Naman Katariya <109310617+katariyanaman@users.noreply.github.com>
2023-01-26 01:24:13 +0530

    Create Graph_representation_using_incidence_list.cpp

---
+++ Graph_representation_using_incidence_list.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+	int i, v, e, j, count;
+ 
+	cout<<"Enter the number of vertexes of the graph: ";
+	cin>>v;
+	cout<<"\nEnter the number of edges of the graph: ";
+	cin>>e;
+	int edge[e][2];
+ 
+	for(i = 0; i < e; i++)
+	{
+		cout<<"\nEnter the vertex pair for edge "<<i+1;
+		cout<<"\nV(1): ";
+		cin>>edge[i][0];
+		cout<<"V(2): ";
+		cin>>edge[i][1];
+	}
+ 
+	cout<<"\n\nThe incidence list representation for the given graph: ";
+	for(i = 0; i < e; i++)
+	{
+		cout<<"\n\tE("<<i+1<<") -> { ";
+		cout<<"V("<<edge[i][0]<<") , "<<"V("<<edge[i][1]<<")";
+		cout<<" }";
+	}
+}
