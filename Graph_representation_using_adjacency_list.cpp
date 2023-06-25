bb37960b54ea00180f71360445d7e49f0268b2cd
Naman Katariya <109310617+katariyanaman@users.noreply.github.com>
2023-06-25 23:35:07 +0530

    Update Graph_representation_using_adjacency_list.cpp

--- Graph_representation_using_adjacency_list.cpp
+++ Graph_representation_using_adjacency_list.cpp
@@ -1,3 +1,6 @@
+/* The following C++ program takes inputs for a graph's number of vertices and edges,
+   and vertex pairs for each edge. It then prints the adjacency list representation of the graph. */
+
 #include<iostream>
  
 using namespace std;
