d413d0bc194d3125336ed9c75164225ee6cc1705
Naman Katariya <109310617+katariyanaman@users.noreply.github.com>
2023-06-28 20:12:15 +0530

    Update Program_to_count_leaf_nodes_in_a_binary_search_tree.cpp

--- Program_to_count_leaf_nodes_in_a_binary_search_tree.cpp
+++ Program_to_count_leaf_nodes_in_a_binary_search_tree.cpp
@@ -1,3 +1,5 @@
+/* The following C++ program counts the number of leaf nodes in three different binary trees and displays the count for each tree. */
+
 #include <iostream>
 using namespace std;
 struct node
