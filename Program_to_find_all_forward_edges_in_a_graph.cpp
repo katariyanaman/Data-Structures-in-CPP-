05c05469558413fe00955d66923b480365f38528
Naman Katariya <109310617+katariyanaman@users.noreply.github.com>
2023-06-29 22:17:48 +0530

    Update Program_to_find_all_forward_edges_in_a_graph.cpp

--- Program_to_find_all_forward_edges_in_a_graph.cpp
+++ Program_to_find_all_forward_edges_in_a_graph.cpp
@@ -1,3 +1,6 @@
+/* The following C++ program performs a topological sort on a 
+   directed graph represented by an adjacency matrix and detects forward edges. */
+
 #include<iostream>
 #include<conio.h>
 using namespace std;
