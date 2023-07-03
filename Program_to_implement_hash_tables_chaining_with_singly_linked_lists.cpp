f1cc14022b53abf111c1bd516c7dc6006c1854fc
Naman Katariya <109310617+katariyanaman@users.noreply.github.com>
2023-07-03 21:15:31 +0530

    Update Program_to_implement_hash_tables_chaining_with_singly_linked_lists.cpp

--- Program_to_implement_hash_tables_chaining_with_singly_linked_lists.cpp
+++ Program_to_implement_hash_tables_chaining_with_singly_linked_lists.cpp
@@ -1,3 +1,7 @@
+/* The following C++ program implements a hash table using separate chaining for
+   collision resolution, allowing operations to insert, search, and delete elements based
+   on a key-value pair. It provides a menu-driven interface for interacting with the hash table. */
+
 #include<iostream>
 #include<cstdlib>
 #include<string>
